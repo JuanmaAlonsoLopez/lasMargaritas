89d801f001e59e14095dc50606e4992c45ca2b40
NegroHm <hh.mauri2190@gmail.com>
2025-05-13 03:22:49 -0300

    cambio de navbar y acomode todas las paginas en modo escritorio, implemente el display grid en listado de productos y sume animaciones varias

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+// Obtenemos el header
+const header = document.querySelector('header');
+
+// Escuchamos el evento de scroll
+window.addEventListener('scroll', () => {
+    // Verificamos si la posición del scroll es mayor que 0
+    if (window.scrollY > 0) {
+        header.classList.add('fixed');
+    } else {
+        header.classList.remove('fixed');
+    }
+});
